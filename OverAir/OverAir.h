5301cc5019df1e247e234da4d98932c9d2bd6dad
Adam Jensen <acj@linuxguy.org>
2014-08-17 15:30:21 -0400

    Initial Commit

---
+++ OverAir/OverAir.h
@@ -0,0 +1,19 @@
+//
+//  OverAir.h
+//  OverAir
+//
+//  Created by Adam Jensen on 8/17/14.
+//
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for OverAir.
+FOUNDATION_EXPORT double OverAirVersionNumber;
+
+//! Project version string for OverAir.
+FOUNDATION_EXPORT const unsigned char OverAirVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OverAir/PublicHeader.h>
+
+
